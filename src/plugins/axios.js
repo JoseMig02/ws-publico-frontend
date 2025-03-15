5a201b3aa073efba6eabd91c4c2614f051af5d97
Jose miguel <martineznene756@gmail.com>
2025-03-15 11:01:28 -0400

    `Update axios baseURL to use VITE_SERVER environment variable`

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:3200/api',
+    baseURL: `${import.meta.env.VITE_SERVER}/api`,
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
